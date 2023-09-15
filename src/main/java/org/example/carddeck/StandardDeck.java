964f0b11dca0e20f42cfebe69fa747b794faacc9
AAPho <austinanpho@gmail.com>
2023-09-14 19:55:27 -0700

    intial commit

--- src/main/java/org/example/carddeck/StandardDeck.java
+++ src/main/java/org/example/carddeck/StandardDeck.java
@@ -1,42 +1,44 @@
 package org.example.carddeck;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class StandardDeck extends AbstractDeck {
 
-    public StandardDeck(){
+    public StandardDeck() {
         this.cards = newDeck();
     }
 
     private List<Card> newDeck() {
-        List<Card> newDeck = new java.util.ArrayList<>();
+
+        List<Card> newDeck = new ArrayList<>();
 
         Arrays.stream(Suit.values()).forEach(suit -> {
             Arrays.stream(FaceValue.values())
                     .map(faceValue -> new Card(suit, faceValue))
-                    .foreach(newDeck::add);
+                    .forEach(newDeck::add);
         });
 
         return cards;
     }
 
     @Override
-    public void shuffle(){
+    public void shuffle() {
         Collections.shuffle(this.cards);
     }
 
     @Override
     public void cut(int index) {
+
         List<Card> top = this.cards.subList(0, index);
-        List<Card> bottom = this.cards.subList(0, this.cards.size());
+        List<Card> bottom = this.cards.subList(index, this.cards.size());
 
         this.cards.clear();
 
         this.cards.addAll(bottom);
         this.cards.addAll(top);
-
     }
 
     @Override
@@ -46,8 +48,8 @@ public class StandardDeck extends AbstractDeck {
 
     @Override
     public void newOrder(Deck deck) {
-        //FIXME: implement
-        //Collections.sort(((AbastractDeck)deck).getDeck());
+        // FIXME: implement
+        // Collections.sort(((AbstractDeck)deck).getDeck());
     }
 
     @Override
--- src/main/java/org/example/carddeck/Suit.java
+++ src/main/java/org/example/carddeck/Suit.java
@@ -1,7 +1,5 @@
 package org.example.carddeck;
 
 public enum Suit {
-    private Suit suit;
-    private FaceValue value;
 
 }
